8f8a65ff6ccacb363f53d3631550d72302860510
WillianAmorim <willian.amorim1@outlook.com>
2021-10-27 18:03:06 -0300

    feito requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import Search from './components/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={ Login } />
+          <Route exact pat="/search" component={ Search } />
+          <Route exact pat="/album/:id" component={ Album } />
+          <Route exact pat="/favorites" component={ Favorites } />
+          <Route exact pat="/profile" component={ Profile } />
+          <Route exact pat="/profile/edit" component={ ProfileEdit } />
+          <Route exact pat="*" component={ NotFound } />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        Componente NotFound
+      </div>
+    );
+  }
+}
+
+export default NotFound;
