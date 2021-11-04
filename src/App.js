b514ba5a876141e8628194e02cc4de3f4f3e7640
WillianAmorim <willian.amorim1@outlook.com>
2021-11-04 15:17:40 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Album from './components/Album';
-import Favorites from './components/Favorites';
-import Login from './components/Login';
-import NotFound from './components/NotFound';
-import Profile from './components/Profile';
-import ProfileEdit from './components/ProfileEdit';
-import Search from './components/Search';
+import Rotas from './components/Rotas';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route exact path="/search" component={ Search } />
-            <Route exact path="/album/:id" component={ Album } />
-            <Route exact path="/favorites" component={ Favorites } />
-            <Route exact path="/profile" component={ Profile } />
-            <Route exact path="/profile/edit" component={ ProfileEdit } />
-            <Route component={ NotFound } />
-          </Switch>
-        </BrowserRouter>
+        <Rotas />
       </div>
     );
   }
---
+++ src/components/Rotas.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Login';
+import Search from './Search';
+import Album from './Album';
+import Favorites from './Favorites';
+import Profile from './Profile';
+import ProfileEdit from './ProfileEdit';
+import NotFound from './NotFound';
+
+class Rotas extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/album/:id">
+            <Album />
+          </Route>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/profile/edit">
+            <ProfileEdit />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Rotas;
